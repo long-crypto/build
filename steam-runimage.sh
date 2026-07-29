6a149c0b55d239c4855eb0dbe3c120fd3c01df5d
long-crypto <175532236+long-crypto@users.noreply.github.com>
2026-07-29 01:51:48 +0000

    Add AppArmor configuration for unprivileged user namespaces

--- steam-runimage.sh
+++ steam-runimage.sh
@@ -30,6 +30,8 @@ run_install() {
 	)
 
 	echo '== checking for updates'
+	sudo tee /etc/sysctl.d/98-apparmor-unuserns.conf <<<kernel.apparmor_restrict_unprivileged_userns=0
+    sudo tee /proc/sys/kernel/apparmor_restrict_unprivileged_userns <<<0 
 	zypper --non-interactive refresh
 
 	echo '== install packages'
